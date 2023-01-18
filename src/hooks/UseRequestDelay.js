1d5150a9e2c11c6c7c823129d8b32cd7f93a177d
Dat Pham <dpham@paramountcommerce.com>
2023-01-18 19:41:27 +0700

    optimistic ui

--- src/hooks/UseRequestDelay.js
+++ src/hooks/UseRequestDelay.js
@@ -9,27 +9,33 @@ const useRequestDelay = (delayTime = 1000, initData = []) => {
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
   useEffect(() => {
+    let status = '';
     const delayFunc = async () => {
       try {
         await delay(delayTime);
-        setRequestStatus(REQUEST_STATUS.SUCCESS);
+        status = REQUEST_STATUS.SUCCESS;
         setData(initData);
       } catch (e) {
-        setRequestStatus(REQUEST_STATUS.FAILURE);
+        status = REQUEST_STATUS.FAILURE;
         setError(e);
       }
     }
-    delayFunc();
+    delayFunc().then(() => {
+      setRequestStatus(status);
+    });
   }, []);
 
   const updateRecord = (recordUpdated, doneCallback) => {
+    const originalRecords = [...data];
     const speakerNewData = data.map(data => data.id === recordUpdated.id ? recordUpdated : data);
     const startDelay = async () => {
       try {
+        setData(speakerNewData); // Update UX data
         await delay(delayTime);
-        setData(speakerNewData);
+        // Update BE logic DB
       } catch (error) {
         console.log("error thrown inside delayFunction", error);
+        setData(originalRecords);
       }
     };
     startDelay().then(() => {
