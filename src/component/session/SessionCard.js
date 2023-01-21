63a091087f7fd37749f51ba953ab03e84f303ead
Dat Pham <dpham@paramountcommerce.com>
2023-01-21 14:28:06 +0700

    add session

--- src/component/session/SessionCard.js
+++ src/component/session/SessionCard.js
@@ -2,7 +2,7 @@ const SessionCard = ({title, room}) => {
   // const {title, room} = props;
   return (
     <span className="session w-100">
-      {title} <strong>Room: {room}</strong>
+      {title} <strong>Room: {room.name}</strong>
     </span>
   )
 }
--- src/component/session/SessionList.js
+++ src/component/session/SessionList.js
@@ -1,12 +1,24 @@
 import SessionCard from "./SessionCard";
+import {useContext} from "react";
+import {SpeakerFilterContext} from "../../context/SpeakerFilterContext";
 
 const SessionList = ({sessions}) => {
+  const {eventYear} = useContext(SpeakerFilterContext);
+  console.log(sessions);
   return (
     <div className="sessionBox card h-250">
       {
-        // sessions.map(s =>
-        <SessionCard title={sessions[0].title} room={sessions[0].room.name}/>
-        // )
+        sessions
+          .filter((session) => {
+            return session.eventYear === eventYear;
+          })
+          .map((session) => {
+            return (
+              <div className="session w-100" key={session.id}>
+                <SessionCard {...session} />
+              </div>
+            );
+          })
       }
     </div>
   );
